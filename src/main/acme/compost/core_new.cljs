946666770622423d0bd3a7f6e298c5d34ab6094b
Jakub Dundalek <dundalek@gmail.com>
2025-03-31 12:27:39 +0200

    wip canvas styling

--- src/main/acme/compost/core_new.cljs
+++ src/main/acme/compost/core_new.cljs
@@ -53,6 +53,7 @@
 ;; SVG stuff
 ;; ------------------------------------------------------------------------------------------------
 
+(declare format-color)
 (declare format-style)
 
 (defn svg-format-path [path]
@@ -90,11 +91,33 @@
       [[:path {:d (svg-format-path p)
                :style (format-style (::definitions ctx) style)}]])))
 
+(defn apply-canvas-style [ctx style]
+  (let [style-clj (style-fs->clj style)
+        {::keys [animation cursor font stroke-color stroke-width fill alignment-baseline text-anchor]} style-clj]
+    (assert (nil? animation) "Animation not implemented")
+    (assert (= cursor "default") "Non-default cursor not implemented")
+    (set! (.-font ctx) font)
+    (let [[so clr] stroke-color
+          [_ sw] stroke-width]
+      (set! (.-strokeStyle ctx) (format-color clr))
+      (set! (.-lineWidth ctx) sw)
+      (when (not= so 0.0)
+        (.stroke ctx)))
+    (case (first fill)
+      ::Solid (let [[_ [fo clr]] fill]
+                (set! (.-fillStyle ctx) (format-color clr))
+                (when (not= fo 0.0)
+                  (.fill ctx))))))
+    ;; TODO
+    ;; alignment-baseline
+    ;; text-anchor
+
 (defn render-canvas [ctx shape]
   (case (first shape)
     ::Text
     (let [[_ [x y] t rotation style] shape]
       (.save ctx)
+      (apply-canvas-style ctx style)
       (if (= rotation 0.0)
         (.fillText ctx t x y)
         (throw (js/Error. "Rotation not implemented yet")))
@@ -110,7 +133,7 @@
       (.save ctx)
       (.beginPath ctx)
       (.ellipse ctx cx cy rx ry 0 0 (* 2 Math/PI))
-      (.stroke ctx)
+      (apply-canvas-style ctx style)
       (.restore ctx))
 
     ::Path
@@ -123,7 +146,8 @@
                      (.moveTo ctx x y))
           ::LineTo (let [[_ [x y]] segment]
                      (.lineTo ctx x y))))
-      (.stroke ctx)
+
+      (apply-canvas-style ctx style)
       (.restore ctx))))
 
 ;; ------------------------------------------------------------------------------------------------
